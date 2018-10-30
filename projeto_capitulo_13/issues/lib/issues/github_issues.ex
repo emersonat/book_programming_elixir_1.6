cf9d358f0c1a2e4dccf4dffc0c4ed146913b276b
emersonat <emersonat@ig.com.br>
2018-10-30 11:13:30 -0300

    add url git

--- projeto_capitulo_13/issues/lib/issues/github_issues.ex
+++ projeto_capitulo_13/issues/lib/issues/github_issues.ex
@@ -1,6 +1,7 @@
 defmodule Issues.GithubIssues do
 
   @user_agent [{"User-agent","Elixir dave@pragprog.com"}]
+  @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
 
@@ -11,7 +12,7 @@ defmodule Issues.GithubIssues do
   end
 
   defp issues_url(user, project) do
-    "https://api.github.com/repos/elixir-lang/elixir/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
   defp handle_response( {_,%{status_code: status_code, body: body} }) do
