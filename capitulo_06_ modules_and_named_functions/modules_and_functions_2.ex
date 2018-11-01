276e06d9be4e52f6a8512cef20b21bc0db15b056
emersonat <emersonat@ig.com.br>
2018-11-01 20:26:46 -0300

    ffdfd

---
+++ capitulo_06_ modules_and_named_functions/modules_and_functions_2.ex
@@ -0,0 +1,8 @@
+$ ​iex times.exs
+iex>​ Times.triple 4
+
+
+$ iex
+iex>​ ​c​ ​"times.exs"
+[Times]
+iex>​ Times.triple 7
---
+++ capitulo_06_ modules_and_named_functions/modules_and_functions_6.ex
@@ -0,0 +1,30 @@
+defmodule Guess do
+  # 1 - 1000
+  #273
+
+  # 500
+  # 250
+  # 375
+  # 312
+  # 281
+  # 265
+  # 273
+
+  def guess(atual, range) do
+
+    meio = div(range,2)
+  end
+
+  def div
+
+  def valida( atual, palpite ) when atual == palpite do
+    atual
+  end
+
+
+
+
+
+
+
+end
